16e21d1c4f07d20e68358c99a432ced2f2228625
Frankie <70291281+costa-frankie@users.noreply.github.com>
2026-03-27 18:55:26 -0700

    Move footer text below graphic, update values

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,12 @@
 export function displayFooterContent() {
     const footer = document.createElement('footer');
-    footer.className = "flex-column justify-end flex-align-center padding-top-xl padding-bottom-md pin-bottom full-width";
     footer.id = "the-footer";
+    footer.classList.add("full-width");
     footer.innerHTML = `
-        <div class="footer-copyright inline-flex gap-xs">
-            <p class="balance-wrap color-on-brand pin-bottom">Created in 2026 by Frankie Costa in the Pacific Northwest </p><span class="material-symbols-outlined inline color-on-brand">forest</span>
+        <div class="flex-column justify-end flex-align-center padding-top-xl padding-bottom-md pin-bottom full-width footer-bg">
+        </div>
+        <div class="flex-row flex-align-center justify-center gap-xs full-width footer-content-bg">
+            <p class="balance-wrap color-on-brand padding-vert-lg pin-bottom">Created in 2026 by Frankie Costa in the Pacific Northwest </p><span class="material-symbols-outlined inline color-on-brand">forest</span>
         </div>
     `;
 
