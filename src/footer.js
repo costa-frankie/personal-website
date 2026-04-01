80bb0a7d8b7dd84354b4d8ed0a754b2d9d168f62
Frankie <70291281+costa-frankie@users.noreply.github.com>
2026-04-01 10:22:00 -0700

    Testing live site deploy

--- src/footer.js
+++ src/footer.js
@@ -6,7 +6,7 @@ export function displayFooterContent() {
         <div class="flex-column justify-end flex-align-center padding-top-xl padding-bottom-md pin-bottom full-width footer-bg">
         </div>
         <div class="flex-row flex-align-center justify-center gap-xs full-width footer-content-bg">
-            <p class="balance-wrap color-on-brand padding-vert-lg pin-bottom">Created in 2026 by Frankie Costa in the Pacific Northwest </p><span class="material-symbols-outlined inline color-on-brand">forest</span>
+            <p class="balance-wrap color-on-brand padding-vert-lg pin-bottom">Created in 2026 by Frankie Costa in the Pacific Northwest 🌲</p>
         </div>
     `;
 
